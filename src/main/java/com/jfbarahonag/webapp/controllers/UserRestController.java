bb84d703bb4c3fb7d0c30221ba43e7b68ac7452f
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-05 14:51:18 -0500

    a rest controlles has been added

---
+++ src/main/java/com/jfbarahonag/webapp/controllers/UserRestController.java
@@ -0,0 +1,31 @@
+package com.jfbarahonag.webapp.controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+  
+  @GetMapping("api/details")
+  public Map<String, Object> details() {
+    Map<String, Object> response = new HashMap<>();
+
+    List<Integer> grades = new ArrayList<>();
+    grades.add(5);
+    grades.add(4);
+
+    response.put("title", "My Title");
+    response.put("h1Title", "This is details page");
+    response.put("username", "J Felipe B Gonza");
+    response.put("age", 33);
+    response.put("grades", grades);
+    
+    return response;
+  }
+
+}
