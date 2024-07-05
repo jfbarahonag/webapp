a7aa41704a572c00fd9ac3c2967aa41728ee68d5
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-05 15:25:32 -0500

    user model data is sent to the controllers (web and rest)

--- src/main/java/com/jfbarahonag/webapp/controllers/UserController.java
+++ src/main/java/com/jfbarahonag/webapp/controllers/UserController.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jfbarahonag.webapp.models.User;
+
 @Controller
 public class UserController {
   
   @GetMapping("/details")
   public String details(Model model) {
+    User user = new User("Juan Felipe", "Barahona Gonzalez", 28);
+    
     model.addAttribute("title", "My Title");
     model.addAttribute("h1Title", "This is details page");
-    model.addAttribute("username", "J Felipe B Gonza");
-    model.addAttribute("age", 33);
+    model.addAttribute("user", user);
     return "details";
   }
 
--- src/main/java/com/jfbarahonag/webapp/controllers/UserRestController.java
+++ src/main/java/com/jfbarahonag/webapp/controllers/UserRestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jfbarahonag.webapp.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
@@ -16,15 +18,13 @@ public class UserRestController {
   @GetMapping("/details")
   public Map<String, Object> details() {
     Map<String, Object> response = new HashMap<>();
+    User user = new User("Juan Felipe", "Barahona Gonzalez", 28);
 
     List<Integer> grades = new ArrayList<>();
     grades.add(5);
     grades.add(4);
 
-    response.put("title", "My Title");
-    response.put("h1Title", "This is details page");
-    response.put("username", "J Felipe B Gonza");
-    response.put("age", 33);
+    response.put("user", user);
     response.put("grades", grades);
     
     return response;
