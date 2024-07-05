01710adfa614ebb736617c99cddb1d6132277f15
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-05 14:31:12 -0500

    passing parammeters to the template

--- src/main/java/com/jfbarahonag/webapp/controllers/UserController.java
+++ src/main/java/com/jfbarahonag/webapp/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.jfbarahonag.webapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
   
   @GetMapping("/details")
-  public String details() {
+  public String details(Model model) {
+    model.addAttribute("title", "My Title");
+    model.addAttribute("h1Title", "This is details page");
+    model.addAttribute("username", "J Felipe B Gonza");
+    model.addAttribute("age", 33);
     return "details";
   }
 
