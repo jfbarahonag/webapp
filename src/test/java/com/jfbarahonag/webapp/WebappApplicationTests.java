cc901e30f4d92b1ccea5b7bb1fd1854f375944c4
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-04 23:27:28 -0500

    First commit

---
+++ src/test/java/com/jfbarahonag/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
