135d0b88ff8e297fa8db4016fb1495b5d9645bf9
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-09 08:00:26 -0500

    adding request params and validations

---
+++ src/main/java/com/jfbarahonag/webapp/controllers/RequestParamsController.java
@@ -0,0 +1,24 @@
+package com.jfbarahonag.webapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jfbarahonag.webapp.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+  @GetMapping("/foo")
+  public ParamDto foo(
+    @RequestParam(required = false, defaultValue = "hey dude, this is a default value") String message) {
+    
+    ParamDto param = new ParamDto();
+    param.setMessage(message);
+    
+    return param;
+  }
+
+}
