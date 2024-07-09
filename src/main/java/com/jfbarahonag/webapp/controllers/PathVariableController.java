618b6ade532abee47ecb28f00b8f850a3c27a284
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-09 17:33:25 -0500

    getting data from environment

--- src/main/java/com/jfbarahonag/webapp/controllers/PathVariableController.java
+++ src/main/java/com/jfbarahonag/webapp/controllers/PathVariableController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +41,9 @@ public class PathVariableController {
   
   @Value("#{${config.valuesMap}.price}")
   private Long price;
+
+  @Autowired
+  private Environment environment;
 
   @GetMapping("/baz/{message}")
   public ParamDto baz(@PathVariable String message) {
@@ -78,6 +83,7 @@ public class PathVariableController {
 
     json.put("username", username);
     json.put("message", message);
+    json.put("messageEnv", environment.getProperty("config.message"));
     json.put("listOfEnvs", listOfEnvs);
     json.put("gradesList", gradesList);
     json.put("valuesMap", valuesMap);
