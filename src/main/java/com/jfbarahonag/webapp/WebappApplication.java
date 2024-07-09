4bf36bc0c60e35b80d32add9926a3907c73b9cc8
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-09 16:12:55 -0500

    custom configurations were moved to values.properties

--- src/main/java/com/jfbarahonag/webapp/WebappApplication.java
+++ src/main/java/com/jfbarahonag/webapp/WebappApplication.java
@@ -2,8 +2,10 @@ package com.jfbarahonag.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:values.properties")
 public class WebappApplication {
 
 	public static void main(String[] args) {
