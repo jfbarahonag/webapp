6fa8a24b53f03fe030c6d8689b36f6efc97f3826
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-09 15:56:10 -0500

    POST endpoint has been added

--- src/main/java/com/jfbarahonag/webapp/controllers/PathVariableController.java
+++ src/main/java/com/jfbarahonag/webapp/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jfbarahonag.webapp.models.User;
 import com.jfbarahonag.webapp.models.dto.ParamDto;
 
 @RestController
@@ -32,5 +35,14 @@ public class PathVariableController {
     json.put("id", id);
 
     return json;
+  }
+
+  @PostMapping("/users")
+  public User createUser(@RequestBody User user) {
+
+    user.setFirstName(user.getFirstName().toUpperCase());
+    user.setLastName(user.getLastName().toUpperCase());
+
+    return user;
   }
 }
